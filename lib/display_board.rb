072f8d9ae50fb5f57575805ccdd0a4a7833c828f
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-09-28 23:18:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,31 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  display_board(board)
+  
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+  display_board(board)
+  
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+  display_board(board)
+  
+board = ["X", " ", " ", " ", "O", " ", " ", " ", " "]
+  display_board(board)
+  
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+  display_board(board)
+  
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+  display_board(board)
+  
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+  display_board(board)
+  
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+  display_board(board)
+  
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
   display_board(board)
   
-  
+board = ["", "", " ", " ", " ", " ", " ", " ", " ", ]
+  display_board(board[0-8 = "X"])
+
+end
